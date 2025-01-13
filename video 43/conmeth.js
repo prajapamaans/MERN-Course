7abbf4d471128c8b3fcbd15b3e542e1e67304fd6
mansi1prajapati <mansiprajapati0003@gmail.com>
2025-01-14 01:26:12 +0530

    add new files

---
+++ video 43/conmeth.js
@@ -0,0 +1,13 @@
+console.log("log")
+console.infp("info")
+console.warn("warn")
+console.error("err")
+console.assert("err" != false)
+console.assert("err" != false)
+
+console.time("forloop")
+for(let i =0;i<5;i++){
+    console.log(455)
+}
+
+console.timeEnd("forloop")
---
+++ video 43/index.js
@@ -0,0 +1,9 @@
+// let arr = [1,2,3,4,5,6]
+// let a = prompt("Enter a Number")
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(a)
+// document.getElementById("demo").innerHTML =
+//     "Hello " + a + "! How are you today?";
+
+
